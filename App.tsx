ad33e707c724852d8fa4e245b215b0e0acefa905
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-06-13 16:10:11 +0530

    Created a form and add post method to update data

--- App.tsx
+++ App.tsx
@@ -1,118 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React ,{useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+ Button,StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TextInput
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App=()=> {
+  const [name,setName]=useState('');
+  const [age,setAge]=useState(0);
+  const [email,setEmail]=useState('');
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const saveData= async()=>{
+    const url="http://10.0.2.2:3000/users";
+    let result=await fetch(url,{
+      method: 'POST',
+      headers:{"Content-Type": "application/json"},
+      body:JSON.stringify({name,age,email})
+   } );
+    result=await result.json();
+    if(result){
+      console.warn("Data added successfully")
+    }
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    
+  }
+   return (
+  <View>
+    <Text style={{fontSize:25}}>Student Details</Text>
+    <TextInput 
+    style={styles.input}
+    placeholder='Enter your name' 
+    value={name} 
+    onChangeText={(text)=>setName(text)} />
+
+<TextInput 
+    style={styles.input}
+    placeholder='Enter your age' 
+    value={age} 
+    onChangeText={(text)=>setAge(text)} />
+
+<TextInput 
+    style={styles.input}
+    placeholder='Enter your email' 
+    value={email} 
+    onChangeText={(text)=>setEmail(text)} />
+    
+
+    <Button title="Submit Details" onPress={saveData} ></Button>
+  </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const styles=StyleSheet.create({
+  input:{
+    borderColor:'skyblue',
+    borderWidth:2,
+    margin:20,
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  }
+})
 
 export default App;
